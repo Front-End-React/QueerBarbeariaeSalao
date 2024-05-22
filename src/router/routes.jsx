3416f47cd6b3bc45aa43b3a1976338cfa35f8085
Dominique Campitelli <dominiquecamp@outlook.com>
2024-05-22 18:24:07 -0300

    Private routes

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,12 +1,17 @@
+import React from "react";
 import {Routes, Route} from 'react-router-dom';
+import PrivateRoutes from "./PrivateRoutes";
+
 import Home from '../pages/Home';
 
-function MainRoutes() {
+export default function MainRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route index path="/" element={<Home />} />
+            <Route path="*" element={<p>Nada aqui!</p>} />
+            <Route element={<PrivateRoutes />}>
+        <Route exact path="/feed" element={<h1>Logado</h1>}></Route>
+      </Route>
         </Routes>
     )
 }
-
-export default MainRoutes;
