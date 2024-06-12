4b2fe40ab06be5656d5996175e998c9650591b29
Dominique Campitelli <dominiquecamp@outlook.com>
2024-06-12 00:14:54 -0300

    Banner, Barber shop and team

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,78 @@
 import React from "react";
 
 import LogoLetter from "../../assets/images/logo_white.png";
+import BannerImage from "../../assets/images/cutting_hair.jpg";
+import BarberShopFacadeGreen from "../../assets/images/barber_shop_facade_green.jpeg";
+import TeamProfessionalPhoto from "../../assets/images/perfil_3.png";
 
-import { Navbar, Logo, Menu, List } from "./styles";
+import {
+  Container,
+  Navbar,
+  Logo,
+  Menu,
+  List,
+  Banner,
+  AboutUs,
+  BarberShopImages,
+  Article,
+  Title,
+  Description,
+  Team,
+  TeamProfessionals,
+  Professional,
+  ProfessionalPhoto,
+  ProfessionalDescription,
+} from "./styles";
 
 export default function Home() {
   return (
-    <Navbar>
-      <Logo>
-        <img src={LogoLetter} alt="Logo Queer" />
-      </Logo>
-      <Menu>
-        <List>Quem somos</List>
-        <List>Equipe</List>
-        <List>Cortes</List>
-        <List>Planos</List>
-        <List>Contato</List>
-        <List>Login</List>
-      </Menu>
-    </Navbar>
+    <Container>
+      <Navbar>
+        <Logo>
+          <img src={LogoLetter} alt="Logo Queer" />
+        </Logo>
+        <Menu>
+          <List>Quem somos</List>
+          <List>Equipe</List>
+          <List>Cortes</List>
+          <List>Planos</List>
+          <List>Contato</List>
+          <List>Login</List>
+        </Menu>
+      </Navbar>
+      <Banner>
+        <img src={BannerImage} alt="Imagem/Banner" />
+      </Banner>
+      <AboutUs>
+        <BarberShopImages>
+          <img
+            src={BarberShopFacadeGreen}
+            alt="Barbearia vista pelo lado de fora"
+          />
+        </BarberShopImages>
+        <Article>
+          <Title>Quem somos</Title>
+          <Description>
+            Inaugurada em 2023 com a proposta de acolher todas as diversidades,
+            trazer conforto e lazer no momento mais importante que é decidir
+            qual visual faz os olhos brilharem mais ao olhar no espelho, seja
+            criar um novo estilo ou manter...
+          </Description>
+        </Article>
+      </AboutUs>
+      <Team>
+        <Title>Nossos profissionais</Title>
+        <TeamProfessionals>
+          <Professional>
+            <ProfessionalPhoto>
+              <img src={TeamProfessionalPhoto} alt="Foto do profissional" />
+            </ProfessionalPhoto>
+            <ProfessionalDescription>
+              Matheus Vince <br /> barbeiro, visagista e colorista
+            </ProfessionalDescription>
+          </Professional>
+        </TeamProfessionals>
+      </Team>
+    </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
 export const Navbar = styled.div`
   width: 100%;
   height: 80px;
-  padding: 10px 20px;
+  padding: 10px 30px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -33,5 +40,102 @@ export const List = styled.li`
   font-family: "Poetsen One";
   font-weight: 400;
   font-size: 18px;
+  color: white;
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  height: 90vh;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const AboutUs = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  padding: 50px;
+  gap: 4rem;
+  background-color: purple;
+`;
+
+export const BarberShopImages = styled.div`
+  width: 400px;
+  height: 500px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Article = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 35%;
+  gap: 1rem;
+`;
+
+export const Title = styled.h1`
+  font-family: "Poetsen One";
+  font-weight: 400;
+  font-size: 24px;
+  color: white;
+`;
+
+export const Description = styled.p`
+  font-family: "Montserrat";
+  font-weight: 400;
+  font-size: 18px;
+  color: white;
+`;
+
+export const Team = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 50px;
+  gap: 2rem;
+  background-color: black;
+`;
+
+export const TeamProfessionals = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  gap: 8rem;
+`;
+
+export const Professional = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  gap: 1rem;
+`;
+
+export const ProfessionalPhoto = styled.div`
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const ProfessionalDescription = styled.div`
+  font-family: "Montserrat";
+  font-weight: 400;
+  font-size: 18px;
+  text-align: center;
   color: white;
 `;
